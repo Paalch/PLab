f4f1680b77f1d42f99286274177edcd9ae1e97f5
Paalch <pciverse@stud.ntnu.no>
2016-03-16 17:41:09 +0100

    Added files via upload

---
+++ zumoRobot.ino
@@ -0,0 +1,90 @@
+z<
+#include <ZumoMotors.h>
+#include <Pushbutton.h>
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
+#include <NewPing.h>
+
+const int echoPin = 8;
+const int triggerPin = 7;
+const int maxDistance = 300;  // default 300 
+NewPing sonar(triggerPin, echoPin, maxDistance);
+
+ 
+#define LED 13
+ 
+// this might need to be tuned for different lighting conditions, surfaces, etc.
+#define QTR_THRESHOLD  1500 // 
+  
+// these might need to be tuned for different motor types
+#define REVERSE_SPEED     400 // 0 is stopped, 400 is full speed
+#define TURN_SPEED        400
+#define FORWARD_SPEED     400
+#define REVERSE_DURATION  200 // ms
+#define TURN_DURATION     300 // ms
+ 
+ZumoMotors motors;
+Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
+ 
+#define NUM_SENSORS 6
+unsigned int sensor_values[NUM_SENSORS];
+ 
+ZumoReflectanceSensorArray sensors;
+
+
+ 
+void setup()
+{
+   sensors.init();
+  // button.waitForButton();
+
+  // for testing 
+   Serial.begin(9600);
+}
+
+void loop()
+{
+ 
+  unsigned int time = sonar.ping();
+  float distance = sonar.convert_cm(time);
+
+  // for testing 
+  Serial.println(distance);
+  delay(50);
+  sensors.read(sensor_values);
+
+  if (sensor_values[0] < QTR_THRESHOLD)
+  {
+    // if leftmost sensor detects line, reverse and turn to the right
+    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    delay(REVERSE_DURATION);
+    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+    delay(TURN_DURATION);
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+  else if (sensor_values[5] < QTR_THRESHOLD)
+  {
+    // if rightmost sensor detects line, reverse and turn to the left
+    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    delay(REVERSE_DURATION);
+    motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+    delay(TURN_DURATION);
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+  else
+  {
+    if(distance>30){
+     // no object detected
+      motors.setSpeeds(0,200);
+    }
+    else{
+    // otherwise, go straight
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+    }
+  }
+}
+
+
+
+
+
