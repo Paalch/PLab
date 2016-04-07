a6beb3c9b128639d3f17182cf1be2646b2f104ac
Paalch <pciverse@stud.ntnu.no>
2016-04-07 14:26:24 +0200

    Added files via upload

---
+++ sketch_apr06a.ino
@@ -0,0 +1,90 @@
+#include <ZumoMotors.h>
+#include <Pushbutton.h>
+#include <QTRSensors.h>
+#include <ZumoReflectanceSensorArray.h>
+
+const int echoPin = 6;
+const int triggerPin = 2;
+const int maxDistance = 300;  // default 300
+
+
+
+#define LED 13
+
+// this might need to be tuned for different lighting conditions, surfaces, etc.
+#define QTR_THRESHOLD  1500 // 
+
+// these might need to be tuned for different motor types
+#define REVERSE_SPEED     400 // 0 is stopped, 400 is full speed
+#define TURN_SPEED        400
+#define FORWARD_SPEED     400
+#define REVERSE_DURATION  200 // ms
+#define TURN_DURATION     300 // ms
+
+ZumoMotors motors;
+Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
+
+#define NUM_SENSORS 6
+unsigned int sensor_values[NUM_SENSORS];
+
+ZumoReflectanceSensorArray sensors;
+
+
+
+void setup()
+{
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  
+  sensors.init();
+  // button.waitForButton();
+
+  // for testing
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(triggerPin, LOW);
+  long duration = pulseIn(echoPin, HIGH);
+  float distance = duration * 0.00017;
+
+  // for testing
+  Serial.println(distance);
+  delay(50);
+  sensors.read(sensor_values);
+/*
+  if (sensor_values[0] < QTR_THRESHOLD)
+  {
+    // if leftmost sensor detects line, reverse and turn to the right
+    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    delay(REVERSE_DURATION);
+    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
+    delay(TURN_DURATION);
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  }
+  else if (sensor_values[5] < QTR_THRESHOLD)
+  {
+    // if rightmost sensor detects line, reverse and turn to the left
+    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
+    delay(REVERSE_DURATION);
+    motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
+    delay(TURN_DURATION);
+    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
+  } else {*/
+    if (distance > 0.2) {
+      // no object detected
+      motors.setSpeeds(-200, 200);
+    } else {
+      // otherwise, go straight
+      motors.setSpeeds(100, 100);
+    }
+  
+}
